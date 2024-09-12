28a462c77d5b7679d8194d6eb2fedc09aaf1cfaa
aramilagros <araapalenque@gmail.com>
2024-09-12 11:03:31 -0300

    update

--- src/components/ItemDessert/index.jsx
+++ src/components/ItemDessert/index.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import estilos from "./estilos.module.css";
-import BtnCart from '../BtnAddCart';
 
 const imagenes = require.context('./img/Desktop', true);
 
 export default function index(props) {
+
+    const [contador, setContador] = useState(1);
+    const [ocultar, setOcultar] = useState(false);
+
+    const sumar = () => {
+        setContador(contador + 1);
+    }
+
+    const restar = () => {
+
+        if(contador == 1) {
+            setOcultar(false)
+        }
+        setContador(contador - 1);
+    }
+
+    const agregarItems = () => {
+        setOcultar(true);
+    }
+
     return (
         <div className={estilos.container}>
 
@@ -12,11 +31,26 @@ export default function index(props) {
                 <div className={estilos.imagen}>
                     <img src={imagenes(props.urlimg)} alt="" />
                 </div>
-                <div className={estilos.btnCart}>
-                    <BtnCart />
-                </div>
+                {!ocultar && (
+                    <div className={estilos.btnCartEmpty} onClick={agregarItems}>
+                        <div className={estilos.cartEmpty}>
+                            <i className="fa-solid fa-cart-plus"></i>
+                            <a>Add to Cart</a>
+                        </div>
+                    </div>
+                )}
+
+                {ocultar && (
+                    <div className={estilos.btnCartFull} >
+                        <div className={estilos.cartFull}>
+                            <i onClick={restar} className="fa-solid fa-minus"></i>
+                            <label>{contador}</label>
+                            <i onClick={sumar} className="fa-solid fa-plus"></i>
+                        </div>
+                    </div>
+                )}
+
             </div>
-        
             <div className={estilos.descripcion}>
                 <label>{props.categoria}</label>
                 <label className={estilos.tittle}>{props.nombre}</label>
